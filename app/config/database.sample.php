322abbf717ab44fa41486402dffdbfe97b0f66ca
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-08-13 19:39:41 +0800

    remove database config and add example file.

--- app/config/database.sample.php
+++ app/config/database.sample.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'motel',
-			'username'  => 'motel',
-			'password'  => '@@motel!@',
+			'database'  => 'database',
+			'username'  => 'root',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
