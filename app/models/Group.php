8e01689084a1ac73faaf2333aa7d4cc09000679a
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-09-04 16:02:30 +0800

    add group model.

--- app/models/Group.php
+++ app/models/Group.php
@@ -1,18 +1,14 @@
 <?php
 
-class UserGroup extends Eloquent {
+class Group extends Eloquent {
 
     /**
      * The database table used by the model.
      *
      * @var string
      */
-    protected $table = 'users_groups';
+    protected $table = 'groups';
     protected $guarded = array('id');
     public $timestamps = false;
 
-    public function user()
-    {
-        return $this->belongsTo('User');
-    }
 }
