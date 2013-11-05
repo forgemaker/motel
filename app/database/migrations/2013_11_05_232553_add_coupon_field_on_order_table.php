d2e763e8406d65511ea272744594484cd0ec3325
Bo-Yi Wu <appleboy.tw@gmail.com>
2013-11-05 23:28:28 +0800

    add coupon filed on orders table.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ app/database/migrations/2013_11_05_232553_add_coupon_field_on_order_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class AddCouponFieldOnOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('orders', function($table) {
+            $table->integer('coupon');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('orders', function($table) {
+            $table->dropColumn('coupon');
+        });
+    }
+
+}
