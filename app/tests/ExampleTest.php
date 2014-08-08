e06ccad4ea2db6a57b9232730fe2d2b6ee61fdcd
Bo-Yi Wu <appleboy.tw@gmail.com>
2014-08-08 15:32:54 +0800

    test user logout.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -2,16 +2,17 @@
 
 class ExampleTest extends TestCase {
 
-	/**
-	 * A basic functional test example.
-	 *
-	 * @return void
-	 */
-	public function testBasicExample()
-	{
-		$crawler = $this->client->request('GET', '/');
-
-		$this->assertTrue($this->client->getResponse()->isOk());
-	}
+    /**
+     * A basic functional test example.
+     *
+     * @return void
+     */
+    public function testBasicExample()
+    {
+        $response = $this->call('GET', '/user/logout');
+        $responseText = json_decode($response->getContent());
+        $this->assertEquals('ok', $responseText->success_text);
+        $this->assertResponseOk();
+    }
 
 }
